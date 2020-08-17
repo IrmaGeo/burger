e86c606064092065318a6a2016677f1f7c6ab099
Irma Georgia <irmamodzg@gmail.com>
2020-08-17 18:17:05 -0400

    deploy heroku

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ app.use(routes);
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+    console.log("Server listening on: https://localhost:" + PORT);
 });
